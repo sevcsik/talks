216d89cad6f06ccd732393169acf6b71ce67c01d
Andras Sevcsik <sevcsik@sevdev.hu>
2017-12-15 16:57:34 +0100

    Read initial logged time from database

--- Reactive Programming with RxJS/app/src/services/log.js
+++ Reactive Programming with RxJS/app/src/services/log.js
@@ -13,15 +13,23 @@ const API_ROOT = 'http://localhost:5984/rxdemo'
 export const logEntries$ = time$
 	.combineLatest(activeTask$, activeTimer$)
 	.filter(([p1, p2, activeTimer]) => activeTimer.running)
-	.map(([time, activeTask, aciveTimer$]) =>
-		({ task: activeTask
-		 , timestamp: time
-		 , duration: INTERVAL
-		 }))
+	.map(([time, activeTask, aciveTimer$]) => ({ task: activeTask
+	                                           , timestamp: time
+	                                           , duration: INTERVAL
+	                                           }))
 
-export const getTotalTimeByTask$ = task => logEntries$
-	.filter(logEntry => task._id === logEntry.task._id)
-	.scan((totalTime, logEntry) => totalTime + logEntry.duration, 0)
+export const getTotalTimeByTask$ = task => {
+	const requestUri = `${API_ROOT}/_design/rxdemo/_view/total_time_by_task?key="${task._id}"`
+	const initial$ = RxHttpRequest.get(requestUri, { json: true })
+		.pluck('body', 'rows', 0, 'value')
+
+	const fromTimer$ = logEntries$
+		.filter(logEntry => task._id === logEntry.task._id)
+		.pluck('duration')
+
+	return Observable.merge(initial$, fromTimer$)
+		.scan((totalTime, duration) => totalTime + duration, 0)
+}
 
 logEntries$
 	.map(({ task
